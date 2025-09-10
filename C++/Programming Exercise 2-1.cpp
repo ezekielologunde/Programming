2e83ba5acbf5c7206f46ee660469b419e7dc713d
Ezekiel Ologunde <156842845+ezekielologunde@users.noreply.github.com>
2025-09-10 00:09:21 -0400

    Create Programming Exercise 2-1.cpp

---
+++ C++/Programming Exercise 2-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "**********************************" << endl;
+  cout << "*    Programming Assignment 1    *" << endl;
+  cout << "*     Computer Programming I     *" << endl;
+  cout << "*   Author: Ezekiel Ologgunde   *"  << endl;
+  cout << "*   Due Date: Thursday, Jan. 24  *" << endl;
+  cout << "**********************************" << endl;
+
+    return 0;
+}
+// This program prints a formatted header to the console.
